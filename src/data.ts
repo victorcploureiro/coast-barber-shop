cd644ea6f60e2c33a637e5a0641a2dda6672a09f
Victor Loureiro <120014871+victorcploureiro@users.noreply.github.com>
2026-09-12 12:13:04 -0300

    Fixed data.ts name missing

--- src/data.ts
+++ src/data.ts
@@ -336,3 +336,6 @@ export const timeSlots = [
   '13:00', '13:30', '14:00', '14:30', '15:00', '15:30',
   '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00'
 ];
+
+// ALIAS PARA COMPATIBILIDADE COM CLUBPAGE.TSX
+export const clubTiers = subscriptionPlans;
